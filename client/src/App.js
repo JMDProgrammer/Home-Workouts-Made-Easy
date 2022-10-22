2ce5f8ac1f4a266e7793b0d851963fa8b9fa0d46
brianp1010 <brianpineda101000@gmail.com>
2022-10-22 15:32:38 -0400

    Login and Registration Skeleton

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+
+  const[usernameReg, setUsernameReg] = useState('')
+  const[passwordReg, setPasswordReg] = useState('')
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Registration">
+          <h1>Registration</h1>
+          <label>Username</label>
+          <input type = "text"/>
+          <label>Password</label>
+          <input type = "text"/>
+          <button>Register</button>
+      </div>
+      <div className="Login">
+          <h1>Login</h1>
+          <label>Username</label>
+          <input type = "text" placeholder='Username...'/>
+          <label>Password</label>
+          <input type = "text" placeholder='Password...'/>
+          <button>Login</button>
+      </div>
     </div>
+
   );
 }
-
 export default App;
