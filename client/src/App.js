d0c41bc3869f292aa2874d0f92665216b9532971
brianp1010 <brianpineda101000@gmail.com>
2022-10-25 15:50:19 -0400

    Frontend and Backend integration with both MySQL and Axios

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,79 @@
-import './App.css';
-import { useState } from 'react';
+//FRONT END WITH AXIOS IMPLEMENTATION
 
+import './App.css';
+import Axios from 'axios';
+import { useState } from 'react';
+import e from 'cors';
 
 function App() {
 
   const[usernameReg, setUsernameReg] = useState('')
   const[passwordReg, setPasswordReg] = useState('')
 
+  const[username, setUsername] = useState('')
+  const[password, setPassword] = useState('')
+
+  const [loginStatus, setLoginStatus] = useState('')
+
+//axios req
+  const register = () => {
+      Axios.post('http://localhost:3001/register', {username: usernameReg, password: passwordReg,
+    }).then((response) => {
+      console.log(response);
+    });
+  }
+
+  const login = () => {
+    Axios.post('http://localhost:3001/login', {username: username, password: password,
+  }).then((response) => {
+
+    if(response.data.message){
+      setLoginStatus(response.data.message)
+    }
+    else{
+      setLoginStatus(response.data[0].username)
+    }
+    console.log(response.data);
+  });
+}
 
 
   return (
+    //registration and login section
     <div className="App">
       <div className="Registration">
           <h1>Registration</h1>
+
           <label>Username</label>
-          <input type = "text"/>
+          <input type = "text" 
+            onChange={(o) => {setUsernameReg(e.target.value);}}/>
+
           <label>Password</label>
-          <input type = "text"/>
-          <button>Register</button>
-      </div>
-      <div className="Login">
-          <h1>Login</h1>
-          <label>Username</label>
-          <input type = "text" placeholder='Username...'/>
-          <label>Password</label>
-          <input type = "text" placeholder='Password...'/>
-          <button>Login</button>
-      </div>
+          <input type = "text"
+            onChange={(o) => {setPasswordReg(e.target.value);}}/>
+
+          <button onClick={register}>Register</button> 
+
     </div>
 
+    <div className="Login">
+
+          <h1>Login</h1>
+          <label>Username</label>
+          <input type = "text" placeholder='Username...'
+          onChange={(o) => {setUsername(e.target.value);}} />
+
+          <label>Password</label>
+          <input type = "text" placeholder='Password...'
+          onChange={(o) => {setPassword(e.target.value);}}/>
+
+          <button onClick={login} >Login</button>
+
+      </div>
+
+    <h1>loginStatus</h1>
+
+    </div>
   );
 }
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,59 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//BACKEND INTEGRATION OF THE LOGIN PAGE. 
+//ALSO CHECKS FOR THE ERRORS IN
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const express = require("express");
+const mysql = require("mysql");
+const cors = require("cors")
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const app = node();
+
+//initializations
+app.use(express.json());
+app.use(cors());
+
+const db = mysql.createConnection({
+  user: "root",
+  host: "localhost",
+  password: "password",
+  database: "LogInSystem",
+});
+
+
+app.post('/register', (req, res) =>{
+
+  const username = req.body.username
+  const password = req.body.password
+  
+  //registers the details in the database.
+  db.query("INSERT INTO users (username, password) VALUES (?,?)", 
+  [username, password], 
+  (err, result) => { console.log(err);}
+  );
+});
+
+app.post('/login' , (req, res) => {
+  const username = req.body.username
+  const password = req.body.password
+
+  //select every element in the database to check if the details are present.
+  db.query("SELECT * FROM users WHERE username = ? AND password = ?", 
+  [username, password], 
+  (err, result) => { 
+      if(err){
+        console.log(err);
+        res.send({err: err})
+      }
+
+      if(result){
+         res.send(result);
+      }
+      else{
+        res.send({message: "Wrong username/password combination!"})
+      }
+      
+  });
+});
+
+app.listen(3001, () => {
+  console.log("running server");
+})
